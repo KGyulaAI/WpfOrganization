d9a1e9282e518e20dacb589df8a6a297fe3a0a21
Gyula <Gyula@DESKTOP-9M6CS2Q>
2023-11-09 23:31:50 +0100

    Add project files.

---
+++ WpfOrganization/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfOrganization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Betoltes("organizations-100000.csv");
+            dgAdatok.ItemsSource = szervezetekLista;
+        }
+        
+        List<Organization> szervezetekLista = new List<Organization>();
+
+        private void Betoltes(string fileName)
+        {
+            foreach (var sor in File.ReadAllLines(fileName).Skip(1))
+            {
+                szervezetekLista.Add(new Organization(sor.Split(';')));
+            }
+        }
+
+        private void dgAdatok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dgAdatok.SelectedItem is Organization)
+            {
+                Organization valasztottObjektum = dgAdatok.SelectedItem as Organization;
+            }
+            else
+            {
+                MessageBox.Show("Hiba!");
+            }
+        }
+    }
+}
